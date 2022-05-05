cfca045e198585af4a3e17f8fc89bf7896029e39
Kaleeswaran Karuppusamy <kalis2050@yahoo.co.in>
2022-05-05 11:41:47 +0800

    mmmm

--- infrastructure_az_cli/create_vm.ps1
+++ infrastructure_az_cli/create_vm.ps1
@@ -83,7 +83,7 @@ Write-Output ""
 
 
 #region Create VM
-# Create a VM in the resource group nnb  dd ddd
+# Create a VM in the resource group nnb
 Write-Output "Creating VM..."
 try {
     az vm create  `
@@ -92,13 +92,13 @@ try {
         --image canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest  `
         --size Standard_E2bds_v5 `
         --admin-username $adminLogin `
-        --ssh-key-value  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGn9gIHax4CldHcr79C2B5+uuq\r\nWAMAEQ5ZBtIIzcM3DsJMJzo/d38ia0/GJiJBJfaKmWEXi42fZz3RwFZYzk36mj9/\r\nQNrNIWQJ5vilZvBZwM7Pkq/QtLyJIW/OlIDKqhNXtyuUNmSrf7f2jv+UTVPeMvec\r\n9fnFRSM662h/yhH2T2kgluC4+792VQOxVJ9bi8Hv3VXEdCDlaX2+xVHEJQIJR9S4\r\nARHMZyCGztHX1UC+E3cmOz9P8TTsfWZUQEeiPXsy4PvdHumn+mLxOJZySPy7vLwB\r\njil5ijddBlcAWskR1WcDLoE8MzXDYG9Mi8jr3qtKvIzgxuc8Kvm+KiTh2rXOvhEV\r\nyKPqJfl8plCQBLHrhzSbvzqz2AXskPztnCWHVFoI7jqh1sZigm/Z4nhJP61aWBwr\r\nwImVOrtuzrEfFJ6CrcoCLDhVKmAVvZgrJo22OUpI64KshtH6OdBhpmLQnXGJGS5j\r\n2ucpr6qgT8G2wvTSGZhv0d20Q1Pm1zUqfo97pcU= generated-by-azure" `
-        --custom-data cloud-init-github.yml `
+        --admin-password   $adminPassword `
+        --custom-data cloud-init-github.txt `
         --public-ip-sku Standard
     }
 
 catch {
-        Write-Output "VM already exists"
+    Write-Output "VM already exists"
     }
 Write-Output "Done creating VM"
 Write-Output ""
@@ -107,4 +107,4 @@ Write-Output ""
 az vm open-port `
   --port 80 `
   --resource-group $resourceGroupName `
-  --name $serverName 
+  --name $serverName
