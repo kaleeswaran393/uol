c4ccde28928b8991d0bb0c43fd2d747e08ee2673
Kaleeswaran Karuppusamy <kalis2050@yahoo.co.in>
2022-05-05 14:30:47 +0800

    mmmm

--- infrastructure_az_cli/create_vm.ps1
+++ infrastructure_az_cli/create_vm.ps1
@@ -88,7 +88,7 @@ Write-Output ""
 
 
 #region Create VM
-# Create a VM in the resource group nnb dd
+# Create a VM in the resource group nnb dd fff
 Write-Output "Creating VM..."
 
 try {
@@ -98,7 +98,7 @@ try {
         --image canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest  `
         --size Standard_E2bds_v5 `
         --admin-username $adminLogin `
-        --ssh-keys-value "'$uolsshkey'" `
+        --ssh-keys-value '$uolsshkey' `
         --custom-data cloud-init-github.txt `
         --public-ip-sku Standard
     }
