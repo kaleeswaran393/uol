0561551988f054e79bea8e496b82355622e61310
Kaleeswaran Karuppusamy <kalis2050@yahoo.co.in>
2022-05-05 18:32:22 +0800

    mm

--- uol_backend_service/routes/programmingLanguages.js
+++ uol_backend_service/routes/programmingLanguages.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const programmingLanguages = require('../services/programmingLanguages');
 
 /* GET programming languages. */
-router.get('/', async function(req, res, next)   {
+router.get('/', async function(req, res, next)     {
   try {
     res.json(await programmingLanguages.getMultiple(req.query.page));
   } catch (err) {
